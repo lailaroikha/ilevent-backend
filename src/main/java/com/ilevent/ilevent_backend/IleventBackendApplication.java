3ac5f51165928930ae2b5b97e7baf7b381fbe944
Laila Roikha <lailajnnh@gmail.com>
2024-06-29 12:13:25 +0700

    Setting up application config

--- src/main/java/com/ilevent/ilevent_backend/IleventBackendApplication.java
+++ src/main/java/com/ilevent/ilevent_backend/IleventBackendApplication.java
@@ -1,11 +1,12 @@
 package com.ilevent.ilevent_backend;
 
+import com.ilevent.ilevent_backend.config.RsaKeyConfigProperties;
 import lombok.extern.java.Log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@EnableConfigurationProperties
+@EnableConfigurationProperties(RsaKeyConfigProperties.class)
 @SpringBootApplication
 @Log
 public class IleventBackendApplication  {
--- src/main/java/com/ilevent/ilevent_backend/config/SecurityConfig.java
+++ src/main/java/com/ilevent/ilevent_backend/config/SecurityConfig.java
@@ -1,14 +1,37 @@
 package com.ilevent.ilevent_backend.config;
 
 import lombok.extern.java.Log;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @Log
 public class SecurityConfig {
+    private final RsaKeyConfigProperties rsaKeyConfigProperties;
 
+    public SecurityConfig(RsaKeyConfigProperties rsaKeyConfigProperties) {
+        this.rsaKeyConfigProperties = rsaKeyConfigProperties;
+
+    }
+
+    @Bean
+    public AuthenticationManager authManager() {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(authProvider);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
