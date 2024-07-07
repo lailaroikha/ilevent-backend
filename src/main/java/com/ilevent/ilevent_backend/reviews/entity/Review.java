44a77247d8c12615a6dfcccab9c28e6a4d256866
Laila Roikha <lailajnnh@gmail.com>
2024-07-07 18:58:21 +0700

    feat(🌟Reviews): add an entity

---
+++ src/main/java/com/ilevent/ilevent_backend/reviews/entity/Review.java
@@ -0,0 +1,44 @@
+package com.ilevent.ilevent_backend.reviews.entity;
+
+import com.ilevent.ilevent_backend.events.entity.Events;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "reviews", schema = "ilevent")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reviews_id_gen")
+    @SequenceGenerator(name = "reviews_id_gen", sequenceName = "reviews_id_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Events event;
+
+    @NotNull
+    @Column(name = "rating", nullable = false)
+    private Integer rating;
+
+    @Column(name = "review", length = Integer.MAX_VALUE)
+    private String review;
+
+    @NotNull
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "deleted_at")
+    private Instant deletedAt;
+
+    @NotNull
+    @Column(name = "update_at", nullable = false)
+    private Instant updateAt;
+
+}
