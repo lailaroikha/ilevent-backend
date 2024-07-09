698bdc0c875b051887216c0c031488f36cba4762
Laila Roikha <lailajnnh@gmail.com>
2024-07-09 11:10:59 +0700

    feat(Price): deleted price

--- src/main/java/com/ilevent/ilevent_backend/price/repository/PriceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ilevent.ilevent_backend.price.repository;
-
-import com.ilevent.ilevent_backend.price.entity.Price;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PriceRepository extends JpaRepository<Price, Long> {
-}
