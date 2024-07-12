e9924aac406e9e77210bc17de808c504fd32bcca
Laila Roikha <lailajnnh@gmail.com>
2024-07-13 06:02:26 +0700

    feat(🌟promo referral): add repository

--- src/main/java/com/ilevent/ilevent_backend/promoReferral/repository/PromoReferralRepository.java
+++ src/main/java/com/ilevent/ilevent_backend/promoReferral/repository/PromoReferralRepository.java
@@ -1,13 +1,13 @@
-//package com.ilevent.ilevent_backend.promoReferral.repository;
-//
-//import com.ilevent.ilevent_backend.promoReferral.entity.PromoReferral;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.Optional;
-//
-//@Repository
-//public interface PromoReferralRepository extends JpaRepository<PromoReferral, Long> {
-//
-//    Optional<PromoReferral> findByEventsIdAndUsersID(Long eventId, Long userId);
-//}
+package com.ilevent.ilevent_backend.promoReferral.repository;
+
+import com.ilevent.ilevent_backend.promoReferral.entity.PromoReferral;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PromoReferralRepository extends JpaRepository<PromoReferral, Long> {
+
+    Optional<PromoReferral> findByEventsId(Long eventId);
+}
