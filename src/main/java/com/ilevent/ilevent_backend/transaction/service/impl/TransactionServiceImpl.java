099b5b6bceee398b6f044b48dd7e3445ca7eacdd
Laila Roikha <lailajnnh@gmail.com>
2024-07-13 10:52:34 +0700

    feat(🌟transaction): add security config for transaction Role for Personal

---
+++ src/main/java/com/ilevent/ilevent_backend/transaction/service/impl/TransactionServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ilevent.ilevent_backend.transaction.service.impl;
+
+import com.ilevent.ilevent_backend.transaction.service.TransactionService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+}
