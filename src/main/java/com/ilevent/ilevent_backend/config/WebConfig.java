146c7bed67e0c9ef8f1e7cf0fb1b3b65520d1233
Laila Roikha <lailajnnh@gmail.com>
2024-07-14 14:52:20 +0700

    feat: web config

--- src/main/java/com/ilevent/ilevent_backend/config/WebConfig.java
+++ src/main/java/com/ilevent/ilevent_backend/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:3000")
+                        .allowedOriginPatterns("http://localhost:3000", "https://ilevent-frontend.example.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
